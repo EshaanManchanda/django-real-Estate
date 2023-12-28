b753510e93f3ef349bd58012a4993a665980fe7d
EshaanManchanda <eshaanmanchanda01@gmail.com>
2023-12-28 13:13:26 +0530

    add custome user model

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -6,3 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('superseceret/', admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real estate admin portal"
+admin.site.index_title="Welcome to the Real estate admin portal"
